0aab985e94e2fe634861fbea418ca5cfd0aa4f14
SARTHAK <47379019+iamxarko@users.noreply.github.com>
2021-03-01 15:29:13 +0530

    Add/update attendance

--- src/app/attendance-management/components/attendance-management/attendance-management.component.ts
+++ src/app/attendance-management/components/attendance-management/attendance-management.component.ts
@@ -14,9 +14,12 @@ export class AttendanceManagementComponent implements OnInit {
 
   attendances: Attendance[] = [];
   subject = new FormControl('HMI', [Validators.required]);
-  date = new FormControl('28-02-2021', [Validators.required]);
+  student = new FormControl('', [Validators.required]);
+  date = new FormControl(null, [Validators.required]);
   options: string[] = ['HMI', 'CCL', 'DC', 'EM', 'NLP'];
   filteredOptions: Observable<string[]> | undefined;
+  studentOptions: any[] = [];
+  filteredStudentOptions: Observable<any[]> | undefined;
   result: any;
 
 
@@ -24,15 +27,23 @@ export class AttendanceManagementComponent implements OnInit {
     this.attendanceManagementService.getAttendance().subscribe(result => {
       this.result = result;
       this.attendances = this.result[this.subject.value][this.date.value];
-      console.log(this.attendances);
+    });
 
+    this.attendanceManagementService.getStudents().subscribe(students => {
+      this.studentOptions = students;
     });
   }
 
   ngOnInit() {
+    this.date.setValue(this.getFormattedDate(new Date()));
     this.filteredOptions = this.subject.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value))
+      map(value => this.filterOptions(value))
+    );
+    this.filteredStudentOptions = this.student.valueChanges.pipe(
+      startWith(''),
+      map((value) => (typeof value === 'string') ? value : value.userId),
+      map((userId) => (userId ? this.filterStudentOptions(userId) : this.studentOptions.slice()) )
     );
     this.subject.valueChanges.subscribe(value => {
       const sub = this.result[value];
@@ -42,22 +53,43 @@ export class AttendanceManagementComponent implements OnInit {
     });
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
+  private filterOptions(value: string): string[] {
+    const filterValue = value ? value.toLowerCase() : '';
     return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  private filterStudentOptions(value: any): any[] {
+    const filterValue = value ? value?.toLowerCase() : '';
+    return this.studentOptions.filter(option => {
+      return option.userId.toLowerCase().startsWith(filterValue);
+    });
   }
 
   onDateChange(strDate: string) {
     const date = new Date(strDate);
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-    const dateResult = [day, month, date.getFullYear()].join('-');
-    console.log(dateResult);
-
     const sub = this.result[this.subject.value];
+    const dateResult = this.getFormattedDate(date);
+    this.date.setValue(dateResult);
     if (sub) {
       this.attendances = sub[dateResult] || [];
     }
+  }
+
+   getFormattedDate = (date: Date) => {
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return [day, month, date.getFullYear()].join('-');
+  }
+
+  displayFn(user: any): string {
+    return user && user.userId && user.name ? `${user.userId}-${user.name}` : '';
+  }
+
+  onPresent = () => {
+    this.attendanceManagementService.markPresent(this.subject.value, this.date.value, this.student.value);
+  }
+
+  onAbsent = () => {
+    this.attendanceManagementService.markAbsent(this.subject.value, this.date.value, this.student.value);
   }
 }
--- src/app/attendance-management/services/attendance-management.service.ts
+++ src/app/attendance-management/services/attendance-management.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttendanceManagementService {
 
-  constructor(private store: AngularFireDatabase) { }
+  constructor(private store: AngularFireDatabase, private snackBar: MatSnackBar) { }
 
   getAttendance = () => {
     return this.store.object<any>(`/attendance`).valueChanges();
+  }
+
+  getStudents = () => {
+    return this.store.list(`/users`, ref => ref.orderByChild('userType').equalTo('student')).valueChanges();
+  }
+
+  markAbsent(subject: any, date: any, student: any) {
+    this.markAttendance(subject, date, student, 'A');
+  }
+  markPresent(subject: any, date: any, student: any) {
+    this.markAttendance(subject, date, student, 'P');
+  }
+
+  markAttendance = (subject: any, date: any, student: any, pOrA: string) => {
+    const attendance = { attendance: pOrA, name: student.name, studentId: student.userId };
+    this.store.object<any>(`/attendance/${subject}/${date}`).update([attendance]).then(() => {
+      this.snackBar.open('Attendance added!', 'Dismiss', {
+        duration: 5000,
+      });
+    }, () => {
+      console.log('Error occurred!');
+    }).catch(() => {
+      console.log('Error occurred!');
+    });
   }
 }
